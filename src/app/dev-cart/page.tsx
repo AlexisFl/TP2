10136ccf279694f36099e499999237c7e65bf7a1
AlexisFlc <falconnet.alexis@gmail.com>
2023-09-27 09:02:41 +0200

    question_1_2

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
+import {ProductCartLine} from "tp-kit/components/products/product-cart-line";
+import {FormattedPrice} from "tp-kit/components/data-display";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
+
 
 export default function DevCartPage() {
     return (
@@ -23,8 +26,30 @@ export default function DevCartPage() {
 
             {/* Panier */}
             <section className="w-full lg:w-1/3 space-y-8">
+                <h2 className="text-lg font-semibold mb-8 tracking-tight">Mon Panier</h2>
+                <div className="text-lg font-semibold">
 
-                <Button variant={"outline"} fullWidth>Vider le panier</Button>
+                        <ProductCartLine
+                            product={products[0]}
+                            qty={1}
+                        />
+
+                </div>
+                    <div className="text-lg font-semibold">
+                        <ProductCartLine
+                            product={products[1]}
+                            qty={2}
+                        />
+                    </div>
+
+                <div className="flex justify-between items-center">
+                    <div className="text-lg font-semibold">Total</div>
+                    <div className="text-lg font-semibold">
+                        <FormattedPrice price={products[0].price + products[1].price * 2} />
+                    </div>
+                </div>
+
+                <Button fullWidth>Commander</Button>
             </section>
             {/* /Panier */}
         </SectionContainer>
