334428b7815d8d5d451a666f6f68b7b2959f9704
AlexisFlc <falconnet.alexis@gmail.com>
2023-09-27 11:53:49 +0200

    question_2_6

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -3,12 +3,21 @@ import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
 import {ProductCartLine} from "tp-kit/components/products/product-cart-line";
 import {FormattedPrice} from "tp-kit/components/data-display";
-import useStore, {addLine} from "../../hooks/use-cart";
+import useStore, {addLine, clearCart, removeLine, updateLine} from "../../hooks/use-cart";
 const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 
 export default function DevCartPage() {
     const lines = useStore((state) => state.lines);
+
+    const handleQtyChange = (line, newQty) => {
+        if (newQty > 0) {
+            updateLine({ ...line, qty: newQty });
+        } else {
+            removeLine(line.product.id);
+        }
+    };
+
 
     return (
         <SectionContainer
@@ -30,30 +39,25 @@ export default function DevCartPage() {
             {/* Panier */}
             <section className="w-full lg:w-1/3 space-y-8">
                 <h2 className="text-lg font-semibold mb-8 tracking-tight">Mon Panier</h2>
-                <pre>{JSON.stringify(lines, null, 2)}</pre>
-                <div className="text-lg font-semibold">
-
-                        <ProductCartLine
-                            product={products[0]}
-                            qty={1}
-                        />
-
-                </div>
-                    <div className="text-lg font-semibold">
-                        <ProductCartLine
-                            product={products[1]}
-                            qty={2}
-                        />
-                    </div>
+                {lines.map((line) => (
+                    <ProductCartLine
+                        key={line.id}
+                        product={line.product}
+                        qty={line.qty}
+                        onQtyChange={(qty) => handleQtyChange(line, qty)}
+                        onDelete={() => removeLine(line.product.id)}
+                    />
+                ))}
 
                 <div className="flex justify-between items-center">
                     <div className="text-lg font-semibold">Total</div>
                     <div className="text-lg font-semibold">
-                        <FormattedPrice price={products[0].price + products[1].price * 2} />
+                        <FormattedPrice price={lines.reduce((acc, line) => acc + line.product.price * line.qty, 0)}/>
                     </div>
                 </div>
 
                 <Button fullWidth>Commander</Button>
+                <Button fullWidth variant="ghost" onClick={() => clearCart()}>Vider le panier</Button>
             </section>
             {/* /Panier */}
         </SectionContainer>
