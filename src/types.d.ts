848a4c073d595bed71385a26b8e7082af7d7d08c
AlexisFlc <falconnet.alexis@gmail.com>
2023-09-27 09:13:04 +0200

    question_2_1

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,24 @@
-import { ProductsCategoryData } from "tp-kit/types"
+import {ProductData, ProductsCategoryData} from "tp-kit/types"
+import internal from "stream";
 
 export type ProductFiltersResult = {
   categoriesSlugs : string[],
   search?: string,
 };
+
+
+export type ProductData = {
+}
+
+export type ProductLineData = {
+  product: ProductData,
+  qty : number,
+}
+
+export type CartData = {
+    lines: ProductLineData[],
+}
+
 
 export type NextPageProps<T = Record<string, string>> = {
   /**
